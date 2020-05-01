6b8797158c4f2c4c4877f65986656853cae93de4
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-05-01 12:22:24 -0300

    scratch.py for informal testing

---
+++ scratch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from pathlib import Path
+
+import numpy as np
+from netCDF4 import Dataset
+
+import sagepy
+
+wmo = 4902481
+brtraj = '/Users/gordonc/Documents/data/Argo/meds/{}/{}_BRtraj.nc'.format(wmo,wmo)
+sprof = brtraj.replace('BRtraj', 'Sprof')
+bnc = Dataset(Path(brtraj), 'r')
+snc = Dataset(Path(sprof), 'r')
