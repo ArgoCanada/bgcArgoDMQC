22cbee1f21351bf6c98d0d53a3df999c1c5d923d
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-05-05 12:50:10 -0300

    example script for demonstrating functions

---
+++ example.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sagepy
+
+# ----------------------------------------------------------------------------
+# example.py - script to demonstrate functionality of sagepy module
+# ----------------------------------------------------------------------------
+#
+#
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# section 0 - setting machine specific variables
+# ----------------------------------------------------------------------------
+# set the paths for data to be saved
+# NOTE: sagepy uses python3's `pathlib` for handling paths, which means that
+# all paths should be formatted in a unix-like way (omit C: on windows and
+# use / instead of \) no matter what your operating system
+
+# define where to save data - these directories must already exists or sagepy
+# will throw errors - can make them by uncommenting lines 32-34
+woa_path  = './WOA18'
+ncep_path = './NCEP'
+argo_path = './Argo'
+
+# uncomment these lines to make the above directories
+# Path(woa_path).mkdir()
+# Path(ncep_path).mkdir()
+# Path(argo_path).mkdir()
+
+# two argo floats - specific floats chosen to use as examples, but can be 
+# altered with no performance change (as long as dac infomation corresponds
+# to float numbers properly)
+float_numbers = [4900497,4902481]
+dac_path = '/ifremer/argo/dac/meds'
+
+# ----------------------------------------------------------------------------
+# section 1 - downloading data
+# ----------------------------------------------------------------------------
+# download WOA and NCEP data necessary for calculating optode gain
+sagepy.io.get_woa18('O2sat', local_path=woa_path) # oxygen % saturation
+sagepy.io.get_ncep('pres', local_path=ncep_path) # surface air pressure in Pa
+
+# download all data for argo floats
+sagepy.io.get_argo(dac_path, float_numbers, local_path=argo_path)
+
+# ----------------------------------------------------------------------------
+# section 2 - loading and interpolating reference data
+# ----------------------------------------------------------------------------
+# load in data from an argo float WITH NO in-air measurements
+float_data_1 = sagepy.load_argo_data(argo_path, float_numbers[0])
+# make 'track' array with columns (time, lat, lon) to be used in interpolation
+track = np.array([float_data_1['SDN'], float_data_1['LATITUDE'], float_data_1['LONGITUDE']]).T
+# get WOA data interpolated along track and associated depth levels
+# NOTE: this function combines sagepy.load_woa_data() and 
+# sagepy.interp_woa_data() for convenience, but each can be called 
+# individually if desired
+z, woa_interp = sagepy.woa_to_float_track(track, 'O2sat', zlim=(0,1000), local_path=woa_path)
+# put interpolated WOA data in dict for use later to calculate gains
+ref_data = dict(z=z, data=woa_interp)
+
+# # load in data from an argo float WITH in-air measurements 
+# float_data_2 = sagepy.load_argo_data(argo_path, float_numbers[1])
+# # put in array for to be used in interpolaton
+# track = np.array([float_data_2['SDN'], float_data_2['LATITUDE'], float_data_2['LONGITUDE']]).T
+# # get NCEP in-air data along track, again this is a convenience function and
+# # sagepy.load_ncep_data() and sagepy.interp_ncep_data() can be called
+# # separately
+# ncep_pres_interp = sagepy.ncep_to_float_track('pres', track, local_path=ncep_path)
+# ncep_rhum_interp = sagepy.ncep_to_float_track('rhum', track, local_path=ncep_path)
+
+# ----------------------------------------------------------------------------
+# section 3 - using the float and reference data to do QC
+# ----------------------------------------------------------------------------
+
+# calculate the gains based on WOA data, also returns the calculated surface
+# data since WOA is depth resolved - specify limit here, default is 25dbar
+woa_gains, surf_data = sagepy.calc_gain(float_data_1, ref_data, inair=False, zlim=25)
+
+# # to calculate the gains based on NCEP data. we first must calculate the 
+# # partial pressure of oxygen from NCEP pressure and humidity
+# ncep_pO2 = sagepy.unit.calc_atmos_pO2(ncep_pres_interp, ncep_rhum_interp)
+# # gain calculation defaults to inair, don't need boolean
+# ncep_gains, inair_data = sagepy.calc_gain(float_data_2, ncep_pO2)
+
+# ----------------------------------------------------------------------------
+# section 4 - plot the results
+# ----------------------------------------------------------------------------
+sdn = float_data_1['SDN']
+fig, axes = plt.subplots(2,1,sharex=True)
+
+g1 = sagepy.plt.float_woa_surface(sdn, surf_data[:,2], woa_interp[0,:], ax=axes[0])
+g2 = sagepy.plt.gains(sdn, woa_gains, inair=False, ax=axes[1])
+
+plt.show()
