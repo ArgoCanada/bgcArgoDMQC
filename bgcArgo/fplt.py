650793b6718739c574d2c96ff086222db7180723
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-02 14:13:19 -0300

    small plotting changes

--- bgcArgo/fplt.py
+++ bgcArgo/fplt.py
@@ -148,32 +148,47 @@ def var_cscatter(df, varname='DOXY', cmap=None, ax=None, ylim=(0,2000), clabel=N
     if cmocean_flag:
         color_maps = dict(
             TEMP=cmo.thermal,
+            TEMP_ADJUSTED=cmo.thermal,
             PSAL=cmo.haline, 
+            PSAL_ADJUSTED=cmo.haline, 
             PDEN=cmo.dense,
             CHLA=cmo.algae,
-            BBP=cmo.matter,
+            CHLA_ADJUSTED=cmo.algae,
+            BBP700=cmo.matter,
+            BBP700_ADJUSTED=cmo.matter,
             DOXY=cmo.ice,
+            DOXY_ADJUSTED=cmo.ice,
             DOWNWELLING_IRRADIANCE=cmo.solar,
         )
     else:
         color_maps = dict(
             TEMP=plt.cm.inferno,
+            TEMP_ADJUSTED=plt.cm.inferno,
             PSAL=plt.cm.viridis, 
+            PSAL_ADJUSTED=plt.cm.viridis, 
             PDEN=plt.cm.cividis,
             CHLA=plt.cm.YlGn,
-            BBP=plt.cm.pink_r,
+            CHLA_ADJUSTED=plt.cm.YlGn,
+            BBP700=plt.cm.pink_r,
+            BBP700_ADJUSTED=plt.cm.pink_r,
             DOXY=plt.cm.YlGnBu_r,
+            DOXY_ADJUSTED=plt.cm.YlGnBu_r,
             DOWNWELLING_IRRADIANCE=plt.cm.magma,
         )
 
     if clabel is None:
         var_units = dict(
             TEMP='Temperature ({}C)'.format(chr(176)),
+            TEMP_ADJUSTED='Temperature ({}C)'.format(chr(176)),
             PSAL='Practical Salinity', 
+            PSAL_ADJUSTED='Practical Salinity', 
             PDEN='Potential Density (kg m${-3}$)',
             CHLA='Chlorophyll (mg m$^{-3}$',
-            BBP='$\mathsf{b_{bp}}$ (m$^{-1}$)',
-            DOXY='Dissolved Oxygen ($\mathregular{\mu}$mol kg$^{-1}$)',
+            CHLA_ADJUSTED='Chlorophyll (mg m$^{-3}$',
+            BBP700='$\mathsf{b_{bp}}$ (m$^{-1}$)',
+            BBP700_ADJUSTED='$\mathsf{b_{bp}}$ (m$^{-1}$)',
+            DOXY='Diss. Oxygen ($\mathregular{\mu}$mol kg$^{-1}$)',
+            DOXY_ADJUSTED='Diss. Oxygen ($\mathregular{\mu}$mol kg$^{-1}$)',
             DOWNWELLING_IRRADIANCE='Downwelling Irradiance (W m$^{-2}$)',
         )
         clabel = var_units[varname]
@@ -220,12 +235,18 @@ def profiles(df, varlist=['DOXY'], Ncycle=1, Nprof=1, zvar='PRES', xlabels=None,
     if xlabels is None:
         var_units = dict(
             TEMP='Temperature ({}C)'.format(chr(176)),
+            TEMP_ADJUSTED='Temperature ({}C)'.format(chr(176)),
             PSAL='Practical Salinity', 
+            PSAL_ADJUSTED='Practical Salinity', 
             PDEN='Potential Density (kg m$^{-3}$)',
             CHLA='Chlorophyll (mg m$^{-3}$',
+            CHLA_ADJUSTED='Chlorophyll (mg m$^{-3}$',
             BBP700='$\mathsf{b_{bp}}$ (m$^{-1}$)',
+            BBP700_ADJUSTED='$\mathsf{b_{bp}}$ (m$^{-1}$)',
             CDOM='CDOM (mg m$^{-3}$)',
+            CDOM_ADJUSTED='CDOM (mg m$^{-3}$)',
             DOXY='Dissolved Oxygen ($\mathregular{\mu}$mol kg$^{-1}$)',
+            DOXY_ADJUSTED='Dissolved Oxygen ($\mathregular{\mu}$mol kg$^{-1}$)',
             DOWNWELLING_IRRADIANCE='Downwelling Irradiance (W m$^{-2}$)',
         )
         xlabels = [var_units[v] for v in varlist]
