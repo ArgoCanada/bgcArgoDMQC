394df1504268c17c2b2ce18ffa6a3d40674a24dc
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-17 13:07:41 -0300

    added response time correction and LUT

--- bgcArgo/core.py
+++ bgcArgo/core.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import numpy as np
 import pylab as pl
+from scipy.interpolate import interp1d, RectBivariateSpline
 
 import matplotlib.pyplot as plt
 
@@ -832,3 +833,70 @@ def get_var_by(v1, v2, float_data):
         out_array[i] = np.nanmean(float_data[v1][float_data[v2] == v])
     
     return out_array
+
+def oxy_b(dt, tau):
+    inv_b = 1 + 2*(tau/dt)
+    return 1/inv_b
+
+def oxy_a(dt, tau):
+    return 1 - 2*oxy_b(dt, tau)
+
+def correct_response_time(t, DO, T, thickness):
+
+    # convert time to seconds
+    t_sec = t*24*60*60
+
+    # array for the loop
+    N = DO.shape[0]
+    mean_oxy  = np.array((N-1)*[np.nan])
+    mean_time = t_sec[:-1] + np.diff(t_sec)/2
+    mean_temp = T[:-1] + np.diff(T)/2
+
+    # load temperature, boundary layer thickness, and tau matrix from 
+    # look-up table provided in the supplement to Bittig and Kortzinger (2017)
+    lut_data = np.loadtxt(Path('LUT/T_lL_tau_3830_4330.dat'))
+    lut_lL = lut_data[0,1:]
+    lut_T  = lut_data[1:,0]
+    tau100 = lut_data[1:,1:]
+    thickness = thickness*np.ones((N-1,))
+
+    # translate boundary layer thickness to temperature dependent tau
+    f_thickness = RectBivariateSpline(lut_T, lut_lL, tau100, kx=1, ky=1)
+    tau_T = np.squeeze(f_thickness(thickness[0], mean_temp))
+
+    # loop through oxygen data
+    for i in range(N-1):
+        dt = t_sec[i+1] - t_sec[i]
+
+        # do the correction using the mean filter, get the mean time
+        mean_oxy[i]  = (1/(2*oxy_b(dt, tau_T[i])))*(DO[i+1] - oxy_a(dt, tau_T[i])*DO[i])
+    
+    # interpolate back to original times for output
+    f = interp1d(mean_time, mean_oxy, kind='linear', bounds_error=False, fill_falue='extrapolate')
+    DO_out = f(t_sec)
+
+    return DO_out
+
+def correct_response_time_Tconst(t, DO, tau):
+
+    # array for the loop
+    N = DO.shape[0]
+    mean_oxy  = np.array((N-1)*[np.nan])
+    mean_time = np.array((N-1)*[np.nan])
+
+    # convert time to seconds
+    t_sec = t*24*60*60
+
+    # loop through oxygen data
+    for i in range(N-1):
+        dt = t_sec[i+1] - t_sec[i]
+
+        # do the correction using the mean filter, get the mean time
+        mean_oxy[i]  = (1/(2*oxy_b(dt, tau)))*(DO[i+1] - oxy_a(dt, tau)*DO[i])
+        mean_time[i] = t_sec[i] + dt/2
+    
+    # interpolate back to original times for output
+    f = interp1d(mean_time, mean_oxy, kind='linear', bounds_error=False, fill_falue='extrapolate')
+    DO_out = f(t_sec)
+
+    return DO_out
