975a69eadb800377614d7148e6d16af1f39a93ea
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-18 11:56:22 -0300

    testing argopy functionality

--- src/scratch.py
+++ src/scratch.py
@@ -6,6 +6,8 @@ import numpy as np
 from netCDF4 import Dataset
 
 import bgcArgo as bgc
+from argopy import DataFetcher as ArgoDataFetcher
+argo_loader = ArgoDataFetcher()
 
-BRtraj = '/Users/gordonc/Documents/data/Argo/meds/4902481/4902481_BRtraj.nc'
-nc = Dataset(Path(BRtraj), 'r')
+# ds = argo_loader.region([-92.06, -62.06, 11.54, 41.54, 0, 1000]).to_xarray()
+ds = argo_loader.float(6902746).to_xarray().to_dataframe()
