8910f45874f3a662706ace544989d044cd641773
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-22 13:22:50 -0300

    basic setup.py, have not run dist yet

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='BGC-QC',
+    version='0.2',
+    packages='bgcArgo',
+    license='The MIT License (MIT)',
+    long_description=open('README.txt').read()
+)
