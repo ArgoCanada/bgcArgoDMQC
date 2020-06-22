2cf2c7fd4cc34961ceaeb693f927e4c67203e297
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-22 15:16:42 -0300

    setup updates

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
     name='BGC-QC',
     version='0.2',
-    packages='bgcArgo',
     license='The MIT License (MIT)',
-    long_description=open('README.txt').read()
+    author_email='chris.gordon@dfo-mpo.gc.ca',
+    description='A python library for quality control of BGC-Argo data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ArgoCanada/BGC-QC',
+    packages=setuptools.find_packages(),
+    package_dir={'argopy': 'argopy'},
+    install_requires=requirements,
+    data_files=[('global_argo_index', ['ref/ar_index_global_meta.txt.gz','ref/ar_index_global_prof.txt.gz']),
+                ('T_lL_tau_LUT',['ref/T_lL_tau_3830_4330.dat'])],
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha'
+    ]
 )
