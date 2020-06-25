cc103c8a6d2a09f820dde56d97d78355bf2891d3
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-06-25 15:51:05 -0300

    first pass at argoFloats comparison

---
+++ src/demonstrate_doxy_adjusted.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from bgcArgo import profiles
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='ticks', palette='colorblind')
+
+datapath = Path('/Users/gordonc/Documents/data/Argo')
+profiles.set_dirs(argo_path=datapath)
+flts = profiles([4902481, 6902905])
+# this function needs to be written!!!
+# flts.clean()
+
+df = flts.to_dataframe()
+df = df[df.DOXY != 99999]
+
+fig, ax = plt.subplots()
+sns.scatterplot(x='DOXY', y='PRES', style='WMO', hue='CYCLE', data=df, ax=ax, linewidth=0.1, alpha=0.5)
+ax.set_xlim((80,340))
+ax.set_ylim((1000,0))
+plt.show()
