8ea4846e5d82810335a897c4d10024a037e0032e
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-07-06 08:57:41 -0300

    download data for imac/matlab running

--- src/dl_validation_data.py
+++ src/dl_validation_data.py
@@ -5,12 +5,14 @@ import pandas as pd
 
 import bgcArgo
 
-wmo = [ 5904852, 5904689, 5904179, 5904682, 4902414, 4901779,
-        4902481, 1901879, 6902905, 6903237, 5903273, 6902805 ]
-dac = 4*['aoml'] + 3*['meds'] + ['bodc'] + 4*['coriolis']
+wmo = [ 5904852, 5904689, 4902414, 4901779,
+        4902481, 1901879, 6902905, 6903237, 6903273, 6902805 ]
+dac = 2*['aoml'] + 3*['meds'] + ['bodc'] + 4*['coriolis']
 
 dacpath = '/ifremer/argo/dac'
 fltpath = ['{}/{}/{}'.format(dacpath, d, w) for d, w in zip(dac, wmo)]
 
 local_path = '/Users/gordonc/Documents/data/Argo'
+# for imac
+local_path = '/Users/ChrisGordon/Desktop/argo/'
 bgcArgo.io.get_argo(fltpath, local_path=local_path)
