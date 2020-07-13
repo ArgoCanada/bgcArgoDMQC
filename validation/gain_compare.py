72a9311cd55d9d8a09260430cc55c554bb6cfa46
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-07-13 08:58:20 -0300

    further validation work, and start of DOXY audit

--- validation/gain_compare.py
+++ validation/gain_compare.py
@@ -12,12 +12,18 @@ from bgcArgo import sprof, profiles
 
 sns.set(context='paper', style='whitegrid', palette='colorblind')
 sage_path = Path('/Users/gordonc/Documents/data/Argo/sage/')
-sage_files = list(sage_path.glob('*float*.h5'))
+sage_files = list(sage_path.glob('*gui*.h5'))
 
-fn = sage_files[0]
-wmo = int(str(fn).split('\\')[-1].split('_')[-1].split('.')[0])
+for fn in sage_files:
+    wmo = int(str(fn).split('\\')[-1].split('_')[-1].split('.')[0])
 
-datapath = Path('/Users/gordonc/Documents/data/')
-profiles.set_dirs(argo_path=datapath / 'Argo', woa_path=datapath / 'WOA18', ncep_path=datapath / 'NCEP')
-syn = sprof(wmo)
-gains = syn.calc_gains(ref='WOA')
+    datapath = Path('/Users/gordonc/Documents/data/')
+    profiles.set_dirs(argo_path=datapath / 'Argo', woa_path=datapath / 'WOA18', ncep_path=datapath / 'NCEP')
+    syn = sprof(wmo)
+    gains = syn.calc_gains(ref='WOA')
+
+    sf = pd.DataFrame(dict(pyCYCLE=syn.CYCLE, pyGAINS=gains))
+    mf = pd.read_hdf(fn)
+    
+    df = pd.merge(sf, mf, left_on='pyCYCLE', right_on='CYCLE')
+    print(df.head())
