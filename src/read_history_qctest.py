bf27c80a0682df9842c13586ab10fec31aa9e9bd
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-07-15 16:49:50 -0300

    code that should go into module
    was looking into reading HISTORY_QCTEST with Dan + Jaimie

---
+++ src/read_history_qctest.py
@@ -0,0 +1,27 @@
+def decode_qctest(hex_code):
+    # hex to numeric
+    num = int(hex_code, 16)
+    # list to save test number in
+    tests = []
+    for i in range(20,0,-1):
+        qc_binary_id = 2**i
+        if i == 20:
+            qc_binary_id -= 4000
+        if qc_binary_id <= num:
+            num -= qc_binary_id
+            tests.append(i)
+    
+    return tests, num
+
+def print_result(tests, remainder):
+    print('Tests performed: ', end='')
+    for t in tests[:-1]:
+        print('{:d}, '.format(t), end='')
+    print('{:d}'.format(tests[-1]))
+    print('Remainder (should be 0): {:d}'.format(remainder))
+
+# get test numbers and print them out
+print('FFBFE:')
+print_result(*decode_qctest('FFBFE'))
+print('\n4000:')
+print_result(*decode_qctest('4000'))
