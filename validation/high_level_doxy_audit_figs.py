ca0f390ad4fb86502d0b5e0dc21b88da16167a41
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-07-21 14:57:31 -0300

    waffle plot added

--- validation/high_level_doxy_audit_figs.py
+++ validation/high_level_doxy_audit_figs.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import seaborn as sns
+from pywaffle import Waffle
 
 # summary comparison between bgcArgo and SAGE/DOXY audit
 fn = Path('../data/doxy_audit_vs_bgcArgo_py_comparison.csv')
@@ -16,7 +17,7 @@ df['diffGAIN'] = np.abs(df.pyGAIN - df.sageGAIN)
 
 # make new dataframe with categories depending on absolute deviation
 counts = pd.DataFrame(dict(N=np.array([df.shape[0],
-                            np.sum(np.isnan(df.pyGAIN)), 
+                            np.sum(df.pyGAIN.isnull()), 
                             df[np.logical_and(df.diffGAIN >= 0.01, df.diffGAIN < 0.05)].shape[0],
                             df[np.logical_and(df.diffGAIN >= 0.05, df.diffGAIN < 0.2)].shape[0],
                             df[df.diffGAIN >= 0.2].shape[0],
@@ -41,4 +42,10 @@ ax.set_title('$N={:d}$'.format(df.shape[0]), loc='left')
 ax.xaxis.set_major_formatter(mtick.PercentFormatter())
 
 plt.savefig('../figures/doxy_audit/DOXY_audit_comparison_breakdown.png', bbox_inches='tight', dpi=250)
+plt.close()
+
+waf = plt.figure(FigureClass=Waffle, rows=5, values=counts[' '].round().values, labels=list(counts.name), colors=tuple(palette), 
+    icons=['check-circle', 'check-circle', 'exclamation-triangle', 'times-circle', 'question-circle', 'infinity'], icon_legend=True, icon_size=10,
+    title={'label': 'Absolute deviation between python and SAGE-O2 gains\nfrom DOXY audit performed by Josh Plant, $N={:d}$'.format(df.shape[0]), 'loc': 'left'}, legend={'loc': 3, 'bbox_to_anchor': (-0.02, -0.5), 'ncol': 3, 'fontsize': 10})
+plt.savefig('../figures/doxy_audit/DOXY_audit_comparison_waffle.png', bbox_inches='tight', dpi=250)
 plt.close()
