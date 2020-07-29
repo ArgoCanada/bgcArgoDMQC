3c6d32f59aa42aaea2cfef2acd83e787523038a3
Chris Gordon <chris.gordon@dal.ca>
2020-07-29 16:21:57 -0300

    resolution to match helm et al

--- src/map_oxygen_profiles.py
+++ src/map_oxygen_profiles.py
@@ -18,15 +18,12 @@ bp = bp[bp.parameters.notna()]
 index = ['DOXY' in parameter_list for parameter_list in bp.parameters]
 doxy = bp[index]
 
-bin_size = 5
+lat_bin_size = 5
+lon_bin_size = 10
+vmax = 4000
 
-if bin_size < 5:
-    vmax = 1000
-else:
-    vmax = 3000
-
-lat_bins = np.arange(-90, 90+bin_size, bin_size)
-lon_bins = np.arange(-180, 180+bin_size, bin_size)
+lat_bins = np.arange(-90, 90+lat_bin_size, lat_bin_size)
+lon_bins = np.arange(-180, 180+lon_bin_size, lon_bin_size)
 
 fig = plt.figure(figsize=(10, 5))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.Robinson(central_longitude=180))
@@ -40,6 +37,6 @@ ax.add_feature(cfeature.BORDERS, zorder=101)
 
 plt.colorbar(h[-1], ax=ax, orientation='horizontal', extend='max', shrink=0.5, pad=0.05, label='$N_{profile}$')
 
-ax.set_title('Number of Oxygen Profiles in each {:d}{}x{:d}{} box'.format(bin_size, chr(176), bin_size, chr(176)))
+ax.set_title('Number of Oxygen Profiles in each {:d}{}x{:d}{} box'.format(lat_bin_size, chr(176), lon_bin_size, chr(176)))
 
-plt.savefig(Path('figures/argo_oxygen_profiles_{:d}x{:d}.png'.format(bin_size, bin_size)), dpi=350, bbox_inches='tight')
+plt.savefig(Path('figures/argo_oxygen_profiles_{:d}x{:d}.png'.format(lat_bin_size, lon_bin_size)), dpi=350, bbox_inches='tight')
