0e1b1c08d38ed2e2a4cebdf9c463631fb7d81e8f
Chris Gordon <chris.gordon@dal.ca>
2020-07-29 14:29:03 -0300

    ran mapping on mac

--- src/map_oxygen_profiles.py
+++ src/map_oxygen_profiles.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from pathlib import Path
+
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -7,6 +9,7 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import seaborn as sns
 import colorcet as cc
+import cmocean.cm as cmo
 
 import bgcArgo as bgc
 
@@ -15,15 +18,28 @@ bp = bp[bp.parameters.notna()]
 index = ['DOXY' in parameter_list for parameter_list in bp.parameters]
 doxy = bp[index]
 
-bin_size = 2
-lat_bins = np.arange(-90, 9+bin_size, bin_size)
-lon_bins = np.arange(-180, 18+bin_size, bin_size)
+bin_size = 10
+
+if bin_size < 5:
+    vmax = 1000
+else:
+    vmax = 4000
+
+lat_bins = np.arange(-90, 90+bin_size, bin_size)
+lon_bins = np.arange(-180, 180+bin_size, bin_size)
 
 fig = plt.figure(figsize=(10, 5))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.Robinson())
+
+h = ax.hist2d(doxy.longitude, doxy.latitude, bins=[lon_bins, lat_bins], cmap=cmo.amp, transform=ccrs.PlateCarree(), vmax=vmax)
+
 ax.set_global()
 ax.coastlines()
-ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.LAND, zorder=100, edgecolor='k')
+ax.add_feature(cfeature.BORDERS, zorder=101)
 
-ax.hist2d(doxy.longitude, doxy.latitude, bins=[lon_bins, lat_bins], cmap=cc.m_fire, transform=ccrs.PlateCarree())
-plt.show()
+plt.colorbar(h[-1], ax=ax, orientation='horizontal', extend='max', shrink=0.5, pad=0.05, label='$N_{profile}$')
+
+ax.set_title('Number of Oxygen Profiles in each {:d}{}x{:d}{} box'.format(bin_size, chr(176), bin_size, chr(176)))
+
+plt.savefig(Path('figures/argo_oxygen_profiles_{:d}x{:d}.png'.format(bin_size, bin_size)), dpi=350, bbox_inches='tight')
