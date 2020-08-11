437503d96a40fd5172663e96d6558c33ee544210
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-08-11 14:35:09 -0300

    running for all current downloaded data

--- validation/dmqc_argo_compare.py
+++ validation/dmqc_argo_compare.py
@@ -9,34 +9,49 @@ import numpy as np
 import pandas as pd
 import bgcArgo as bgc
 
+def get_all_wmos():
+
+    argo_path = Path('/Users/gordonc/Documents/data/Argo/')
+    wmos = []
+    dacs = list(argo_path.glob('*'))
+    dacs.pop(-1)
+    for dac in dacs:
+        wmos = wmos + list(dac.glob('*'))
+
+    wmos = [int(p.as_posix().split('/')[-1]) for p in wmos]
+    
+    return wmos
+
 argopath = '/Users/gordonc/Documents/data/Argo/'
 bgc.set_dirs(argo_path=argopath, woa_path='/Users/gordonc/Documents/data/WOA18/')
-wmos = [3900345]
+wmos = get_all_wmos()
 
 for wmo in wmos:
-    syn = bgc.sprof(wmo)
-    gains = syn.calc_gains(ref='WOA')
-
     files = bgc.get_files(local_path=argopath, wmo_numbers=[wmo], mission='B', mode='D')
 
-    file_time  = np.array(len(files)*[np.nan])
-    file_gains = np.array(len(files)*[np.nan])
-    file_msgs  = np.array(len(files)*[256*' '])
-    syn_gains  = np.array(len(files)*[np.nan])
+    if len(files) > 0:
+        syn = bgc.sprof(wmo)
+        gains = syn.calc_gains(ref='WOA')
 
-    for i,fn in enumerate(files):
-        nc = Dataset(Path(fn))
-        gain, msg = bgc.util.read_gain_value(nc)
-        c = nc.variables['CYCLE_NUMBER'][:][0]
-        if c > syn.CYCLE[-1]:
-            syn_gain = np.nan
-        else:
-            syn_gain = gains[syn.CYCLE == c][0]
+        file_time  = np.array(len(files)*[np.nan])
+        file_gains = np.array(len(files)*[np.nan])
+        file_msgs  = np.array(len(files)*[256*' '])
+        syn_gains  = np.array(len(files)*[np.nan])
 
-        file_time[i]  = nc.variables['JULD'][:][0]
-        file_gains[i] = gain
-        file_msgs[i]  = msg
-        syn_gains[i]  = syn_gain
-    
-    diffs = syn_gains - file_gains
-    
+        for i,fn in enumerate(files):
+            nc = Dataset(Path(fn))
+            gain, msg = bgc.util.read_gain_value(nc)
+            c = nc.variables['CYCLE_NUMBER'][:][0]
+            if c > syn.CYCLE[-1]:
+                syn_gain = np.nan
+            else:
+                syn_gain = gains[syn.CYCLE == c][0]
+
+            file_time[i]  = nc.variables['JULD'][:][0]
+            file_gains[i] = gain
+            file_msgs[i]  = msg
+            syn_gains[i]  = syn_gain
+        
+        diffs = syn_gains - file_gains
+
+        print(diffs)
