9a0ebef43b749c58b433c2deef803f36bca1179e
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-08-18 15:39:01 -0300

    downloading delayed mode data

--- src/dl_validation_data.py
+++ src/dl_validation_data.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
 
+from pathlib import Path
 import numpy as np
 import pandas as pd
 
-import bgcArgo
+import bgcArgo as bgc
 
-wmo = [ 5904179, 5904682, 4902414, 4901779,
-        4902481, 1901879, 6902905, 6903237, 6903273, 6902805 ]
-dac = 2*['aoml'] + 3*['meds'] + ['bodc'] + 4*['coriolis']
+def get_all_wmos():
 
-wmo = [3900407]
-dac = ['aoml']
+    argo_path = Path('/Users/gordonc/Documents/data/Argo/')
+    wmos = []
+    dacs = list(argo_path.glob('*'))
+    dacs.pop(-1)
+    for dac in dacs:
+        wmos = wmos + list(dac.glob('*'))
+
+    wmos = [int(p.as_posix().split('/')[-1]) for p in wmos]
+    
+    return wmos
+
+wmo = get_all_wmos()
+dac = [bgc.get_dac(w) for w in wmo]
 
 dacpath = '/ifremer/argo/dac'
 fltpath = ['{}/{}/{}'.format(dacpath, d, w) for d, w in zip(dac, wmo)]
@@ -18,4 +28,4 @@ fltpath = ['{}/{}/{}'.format(dacpath, d, w) for d, w in zip(dac, wmo)]
 local_path = '/Users/gordonc/Documents/data/Argo'
 # for imac
 # local_path = '/Users/ChrisGordon/Desktop/argo/'
-bgcArgo.io.get_argo(fltpath, local_path=local_path)
+bgc.io.get_argo(fltpath, local_path=local_path, mission='B', mode='D')
