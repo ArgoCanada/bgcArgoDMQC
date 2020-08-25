a8f206ddacff9e4951eb4620f5b449aa947ec1e7
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-08-25 10:33:01 -0300

    remove print statements used in troubleshooting

--- bgcArgo/io.py
+++ bgcArgo/io.py
@@ -675,10 +675,8 @@ def load_ncep_data(track, varname, local_path='./'):
         ncep_file = local_path / varname / '{}.{}.nc'.format(base_file, y)
         nc = Dataset(ncep_file, 'r')
 
-        print(ncep_file)
         time = nc.variables['time'][:]
         time = time/24 + pl.datestr2num('1800-01-01')
-        print(time.shape)
 
         if y == yrs[0]:
             lat = nc.variables['lat'][:]
@@ -704,12 +702,10 @@ def load_ncep_data(track, varname, local_path='./'):
                 ncep_file_test = local_path / varname / '{}.{}.nc'.format(base_file, ytest)
                 nctest = Dataset(ncep_file_test, 'r')
                 extra_time_indices += nctest.variables['time'].shape[0] - len(time)
-            print(extra_time_indices)
 
             landmask = lnc.variables['land'][:][0,:,:][:,lon_ix][lat_ix,:].astype(bool)
             ncep_time = np.nan*np.ones((len(time)*(Nyear+1)+extra_time_indices))
             data = np.nan*np.ones((len(time)*(Nyear+1)+extra_time_indices, len(lat_sub), len(lon_sub)))
-            print(data.shape)
 
         vdata = nc.variables[varname][:]
         for i in range(len(time)):
