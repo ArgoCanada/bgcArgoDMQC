b40b99107a060b348e7bce3cc97ca1c8c0d370c1
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-11-30 15:53:15 -0400

    latest audit file

--- validation/doxy_audit_compare.py
+++ validation/doxy_audit_compare.py
@@ -2,22 +2,18 @@
 
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-
 import bgcArgoDMQC as bgc
 
 # load in DOXY audit file - most recent one was on July 9 2020
-audit_file = Path('../data/DOXY_audit_070720.TXT')
+audit_file = Path('/Users/gordonc/Documents/argo/doxy-audit/DOXY_audit_112020.TXT')
 df = pd.read_csv(audit_file, sep='\t', header=25)
 
 # download the synthetic, meta, and BRtraj files for each float in the audit
 dacpath = '/ifremer/argo/dac'
 fltpath = ['{}/{}/{}'.format(dacpath, bgc.get_dac(w), w) for w in df.WMO.unique()]
 local_path = '/Users/gordonc/Documents/data/Argo'
-# bgc.io.get_argo(fltpath, local_path=local_path, mode='summary')
+bgc.io.get_argo(fltpath, local_path=local_path, mode='summary')
 bgc.set_dirs(argo_path='/Users/gordonc/Documents/data/Argo', woa_path='/Users/gordonc/Documents/data/WOA18')
 
 with open(Path('../data/doxy_audit_vs_bgcArgo_py_comparison_20200920.csv'),'w') as fid:
