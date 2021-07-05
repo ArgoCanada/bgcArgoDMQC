17da0aa6db342e17b8980c640d25abe2872e8821
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-07-05 16:47:16 -0300

    initial netcdf ouline

---
+++ bgcArgoDMQC/netcdf.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+from pathlib import Path
+from netCDF4 import Dataset
+
+def populate_flags():
+    '''
+    Args:
+        arg: description
+
+    Returns:
+        out: description
+
+    Author:
+        Christopher Gordon
+        Fisheries and Oceans Canada
+        chris.gordon@dfo-mpo.gc.ca
+
+    Change log:
+        - 2021-07-05: initial commit
+    '''
+
+def generate_comments_equations(variable, gain=None, operator='[operator name]', affiliation='[operator affiliation]', orcid=''):
+    '''
+    Args:
+        arg: description
+
+    Returns:
+        out: description
+
+    Author:
+        Christopher Gordon
+        Fisheries and Oceans Canada
+        chris.gordon@dfo-mpo.gc.ca
+
+    Change log:
+        - 2021-07-05: initial commit
+    '''
+
+def adjust_flags(index, flags):
+    '''
+    Args:
+        arg: description
+
+    Returns:
+        out: description
+
+    Author:
+        Christopher Gordon
+        Fisheries and Oceans Canada
+        chris.gordon@dfo-mpo.gc.ca
+
+    Change log:
+        - 2021-07-05: initial commit
+    '''
+
+def fill_string_array(s):
+    '''
+    Args:
+        arg: description
+
+    Returns:
+        out: description
+
+    Author:
+        Christopher Gordon
+        Fisheries and Oceans Canada
+        chris.gordon@dfo-mpo.gc.ca
+
+    Change log:
+        - 2021-07-05: initial commit
+    '''
