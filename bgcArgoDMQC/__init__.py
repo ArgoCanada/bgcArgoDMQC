f4e8142712a886254a2e048b21f137bde9574186
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2023-01-26 07:56:24 -0400

    changelog out of date and not used

--- bgcArgoDMQC/__init__.py
+++ bgcArgoDMQC/__init__.py
@@ -9,22 +9,6 @@
 - [pandas](https://pandas.pydata.org/) is required (and highly recommended for all your data science needs!)
 - [seaborn](https://seaborn.pydata.org/) is recommended but not required, through there will be some reduced (non-essential) functionality
 - [cmocean](https://matplotlib.org/cmocean/) is also recommended for nicer plots, but not required
-
-## version history
-
-0.1: April 20, 2020 - Initial creation
-
-0.2: May 13, 2020 - Major change to how end user would use module, change to more object-oriented, create argo class
-
-0.2.1: June 23, 2020 - pandas is now required, makes reading of global index significantly easier and more efficient
-
-0.2.2: August 28, 2020 - remove pylab dependency (is part of matplotlib), built and uploaded to PyPI, build conda-forge recipe
-
-0.2.3 - 0.2.6: September 3, 2020 - updates to pass all checks on conda-forge pull request, updated on PyPI as well
-
-0.2.7 - 0.2.8: September 29, 2020 - re-spun for PyPI and PR to conda-feedstock
-
-0.2.9: November 9, 2020 - name change to bgcArgoDMQC, various other updates over the past month
 '''
 
 from __future__ import absolute_import
@@ -37,13 +21,12 @@ from . import unit
 from . import util
 from . import io
 from . import interp
-from . import diagnostic
 
-__all__ = ['plot', 'unit', 'util', 'io', 'interp', 'diagnostic', 'configure']
+__all__ = ['plot', 'unit', 'util', 'io', 'interp', 'configure']
 
 __author__ = ['Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>']
 
-__version__ = '0.2.9'
+__version__ = '0.2.13'
 
 # check age of index file, or if it exists
 if not io.index_exists():
