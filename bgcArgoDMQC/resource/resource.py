729fb6b191e29321f005e38e4684a78acaca13c2
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2023-01-31 15:36:59 -0400

    see what these look like in actions

--- bgcArgoDMQC/resource/resource.py
+++ bgcArgoDMQC/resource/resource.py
@@ -6,4 +6,6 @@ def path(loc):
     local_absolute = Path(__file__).parent.absolute()
     loc = 'Argo/dac' if loc=='Argo' else loc
 
+    print(local_absolute / loc)
+
     return local_absolute / loc
