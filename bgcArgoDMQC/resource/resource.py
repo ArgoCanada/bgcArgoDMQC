df2a85b13a44379d1b2f2c2ec466a24bf3e103d7
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2023-02-02 11:34:11 -0400

    move data, attempt to include in package

--- bgcArgoDMQC/resource/resource.py
+++ bgcArgoDMQC/resource/resource.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def path(loc):
 
-    local_absolute = Path(__file__).parent.absolute()
+    local_absolute = Path(__file__).parent.absolute() / 'data'
     loc = 'Argo/dac' if loc=='Argo' else loc
 
     return local_absolute / loc
